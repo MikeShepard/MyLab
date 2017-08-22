2c5cfc25987b1c9c6c08895e275cebbce366bce8
Mike Shepard <mshepard70@gmail.com>
2017-08-22 06:56:22 -0500

    Got some of the unattend working

--- MyLab.psm1
+++ MyLab.psm1
@@ -7,13 +7,14 @@ $MasterVhdFolder = Join-Path -Path $LabRoot -ChildPath Master
 
 $propertyRouting=@{
                     VM=@('Memory'
-                         'vCPpu',
+                         'vCpu',
                          'ISO',
                          'Edition'
-                         'SizeInBytes')
+                         'SizeInBytes',
+                         'Switch')
                     Unattend=@(
                         'ComputerName',
-                        'AdminPassword',
+                        'AdminPwd',
                         'UserName',
                         'UserPassword'
                         'IPAddress',
@@ -25,28 +26,35 @@ $propertyRouting=@{
                   
 }
 function New-MyLab {
-    [CmdletBinding(SupportsShouldProcess=$true)]
+    [CmdletBinding(SupportsShouldProcess = $true)]
     Param([ArgumentToConfigurationDataTransformation()]
-          $config
+        $config
     )
     
     end {
-        foreach($node in $config.Nodes){
-            $nodeConfig=$config.AllNodes.Clone()
-            $nodeConfig.Name='{0}-{1}' -f $config.LabName,$node.Name
-            foreach($item in $propertyRouting.VM){
-                 if($node.ContainsKey($item)){}
-                    $nodeConfig[$item]=$node[$item]
-                }
+        foreach ($node in $config.Nodes) {
+            $nodeConfig = @{}
+            $nodeConfig.Name = '{0}-{1}' -f $config.LabName, $node.Name
+            foreach ($item in $propertyRouting.VM) {
+                if ($config.AllNodes.ContainsKey($item)) {}
+                $nodeConfig[$item] = $config.AllNodes[$item]
             }
+            foreach ($item in $propertyRouting.VM) {
+                if ($node.ContainsKey($item)) {
+                    $nodeConfig[$item] = $node[$item]
+                }
+            
+            }
+
             write-verbose "Done creating parameters for $($nodeconfig.Name) configuration"
             write-verbose ($nodeConfig | out-string)
-            if($PSCmdlet.ShouldProcess($nodeconfig.name,'Create VM')){
+            if ($PSCmdlet.ShouldProcess($nodeconfig.name, 'Create VM')) {
                 new-BootableVM @nodeconfig -Stopped
-            }
+             }
         }
     }
 }
+
 
 
 function Remove-MyLab{
@@ -101,10 +109,10 @@ function new-BootableVM {
             $vCPUs,
             $edition,
             $sizeInBytes,
+            $Switch,
             [switch]$Stopped)
      
      
-        $switch = 'LabNet'
         
         #if there isn't a matching master, create it
 
@@ -119,10 +127,11 @@ function new-BootableVM {
         #create a differencing disk for the new VM
         $ThisVHDPath=join-path $vhdpath "$Name.vhdx"
         New-VHD -Path $ThisVHDPath -ParentPath $MasterPath -Differencing  
+        new-unattendFile -config $config -node $node -ThisVhdPath $ThisVHDPath
         $vm = New-VM -Name $name -MemoryStartupBytes ($memoryInGB * 1GB)  -Generation 2 -VHDPath $ThisVHDPath
         Set-VMProcessor -VM $vm -Count $vCPUs
         Add-VMNetworkAdapter -vm $vm -SwitchName $switch
-     
+        
         if (!$stopped) {
             Start-VM -VM $vm
         }
@@ -130,44 +139,30 @@ function new-BootableVM {
     }
 
     function new-unattendFile{
-        $Name="Test"
-        #IP Address
-        $IPDomain="192.168.0.1"
-        #Default Gateway to be used
-        $DefaultGW="192.168.0.254"
-        #DNS Server
-        $DNSServer="192.168.0.1"
-        #DNS Domain Name
-        $DNSDomain="test.com"
-        #User name and Password
-        $AdminAccount="Administrator"
-        $AdminPassword="P@ssw0rd"
-        #Org info
-        $Organization="Test Organization"
-        #This ProductID is actually the AVMA key provided by MS
-        $ProductID="TMJ3Y-NTRTM-FJYXT-T22BY-CWG3J"
-        #Create a temp file name
-        $tempfile=New-TemporaryFile 
-        Copy-item -Path $PSScriptRoot\unattend_2016.xml -Destination $tempfile
-        #copy the "proper" unattend file to the temp file
-        #replace the values in the temp file
-        #mount the vhdx
-        #copy the unattend
-        #unmount the vhdx
-        #delete the temp file
+        [CmdletBinding()]
+        Param($config,
+              $node,
+              $ThisVhdPath)
+        $tempfile=New-TemporaryFile
         write-host "Writing unattend.xml from $tempfile"
-        $DefaultXML=Get-Content $tempfile
-        $DefaultXML  | Foreach-Object {
-         $_ -replace '1AdminAccount', $AdminAccount `
-         -replace '1Organization', $Organization `
-         -replace '1Name', $Name `
-         -replace '1ProductID', $ProductID`
-         -replace '1MacAddressDomain',$MACAddress `
-         -replace '1DefaultGW', $DefaultGW `
-         -replace '1DNSServer', $DNSServer `
-         -replace '1DNSDomain', $DNSDomain `
-         -replace '1AdminPassword', $AdminPassword `
-         -replace '1IPDomain', $IPDomain `
-         } | Set-Content c:\temp\unattend.xml
-         
+        $DefaultXML=Get-Content $PSScriptRoot\unattend.xml -Raw
+        foreach($item in $propertyRouting.Unattend){
+            $propertyValue='NOT_SET'
+            if($config.AllNodes.ContainsKey($item)){
+                $propertyValue=$config.AllNodes[$item]
+            }
+            if($node.ContainsKey($item)){
+                $propertyValue=$node[$item]
+            }
+            if($propertyValue -ne 'NOT_SET'){
+                $defaultXML=$defaultXML -replace "#$item#",$propertyValue
+            }
+            
+        }
+        Set-Content -Path $tempfile -Value $DefaultXML
+        $VHDDriveLetter = (Mount-VHD $ThisVhdPath -Passthru | Get-Disk | Get-Partition | ?{$_.Type -eq "Basic" }).DriveLetter
+        start-sleep -Seconds 2
+        Copy-item -Path $tempfile -Destination "${VhdDriveLetter}:\Unattend.xml"
+        Dismount-VHD -Path $ThisVhdPath
+
     }
--- Sample/DSCLab/DSCLab.psd1
+++ Sample/DSCLab/DSCLab.psd1
@@ -6,15 +6,19 @@
         ISO         = '2016_x64_EN_Eval.iso'
         Edition     = 'ServerDataCenterEvalCore'
         SizeInBytes = 10GB
+        Switch='Labnet'
+        AdminPwd='C@lamity'
     }
     Nodes    = @(
         @{
             Name = 'DC'
+            ComputerName='LabDC01'
             Memory=1.5
         }
         @{
             Name = 'Web1'
             Edition='SERVERDATACENTER'
+            ComputerName='LabWeb01'
         }
     )
 }
